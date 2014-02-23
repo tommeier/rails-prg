98cae02c50ce0d15dd2f144f394ebdfccab8ec69
Tom Meier <tom@venombytes.com>
2014-02-24 07:28:51 +1100

    Create feature stubs

---
+++ spec/rails/prg/features/error_duplication_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+feature "Display error based on browser loaded to ensure error still occours in later Rails versions" do
+  # scenario "User creates a new widget" do
+  #   visit "/widgets/new"
+
+  #   fill_in "Name", :with => "My Widget"
+  #   click_button "Create Widget"
+
+  #   expect(page).to have_text("Widget was successfully created.")
+  # end
+end
---
+++ spec/rails/prg/features/redirected_objects_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+feature "Display redirection and display of form content and errors" do
+  # scenario "User creates a new widget" do
+  #   visit "/widgets/new"
+
+  #   fill_in "Name", :with => "My Widget"
+  #   click_button "Create Widget"
+
+  #   expect(page).to have_text("Widget was successfully created.")
+  # end
+end
